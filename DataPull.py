0547ce02f0be0b4d3cdc04e01a4a7d706413e15f
Mason Tian <masontian@nat-tvwna-inside-visitornet101-b-3971.princeton.org>
2017-11-16 15:10:42 -0500

    Ver 2

--- DataPull.py
+++ DataPull.py
@@ -6,16 +6,6 @@ import datetime
 import csv
 from datapackage import Package
 
-package = Package('http://datahub.io/core/s-and-p-500-companies/datapackage.json')
-
-# get list of resources:
-resources = package.descriptor['resources']
-resourceList = [resources[x]['name'] for x in range(0, len(resources))]
-print(resourceList)
-
-data = package.resources[0].read()
-print(data)
-
 def get_price_data(t):
     data = quandl.get('WIKI/'+t)
     return data
@@ -29,7 +19,21 @@ def main():
     return
 
 
-def get_all_tickers():
+def get_today():
+    today = datetime.datetime.now()
+    date_today = str(today.year)
+    if today.month < 10:
+        date_today += '-0' + str(today.month) + '-'
+    else:
+        date_today += '-' + str(today.month) + '-'
+    if today.day < 10:
+        date_today += '0' + str(today.day)
+    else:
+        date_today += str(today.day)
+    return date_today
+
+
+def get_yesterday():
     today = datetime.datetime.now()
     yesterday = str(today.year)
     if today.month < 10:
@@ -40,10 +44,18 @@ def get_all_tickers():
         yesterday += '0' + str(today.day)
     else:
         yesterday += str(today.day - 1)
-    print(yesterday)
-    data = quandl.get('WIKI', paginate=True)
+    return yesterday
+
+def get_all_tickers():
+    package = Package('http://datahub.io/core/s-and-p-500-companies/datapackage.json')
+    resources = package.descriptor['resources']
+    resourceList = [resources[x]['name'] for x in range(0, len(resources))]
+    print(resourceList)
+    data = package.resources[0].read()
     print(data)
-    return data
+    tickers = []
+    return tickers
+
 
 if __name__ == "__main__":
     quandl.ApiConfig.api_key = "RxidFKB69HRV8VFHbXqM"
