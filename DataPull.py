d2eaa83b399b8a71dc02e934d01e771de2fdd145
Mason Tian <masontian@nat-tvwna-inside-visitornet101-b-3971.princeton.org>
2017-11-17 16:56:05 -0500

    SMA code V2
    Works for periods within weekdays

--- DataPull.py
+++ DataPull.py
@@ -7,63 +7,33 @@ import csv
 from datapackage import Package
 
 
+quandl.ApiConfig.api_key = "RxidFKB69HRV8VFHbXqM"
+
+
 def get_data(t):
     data = quandl.get('WIKI/'+t)
     return data
 
 
 def get_price_data(t, start, end):
-    data = quandl.get('Wiki/'+ t + '.4', start_date=start, end_date=end)
+    start_date = format_date(start)
+    end_date = format_date(end)
+    data = quandl.get('Wiki/'+ t, start_date=start_date, end_date=end_date)
+    print(data)
     return data
 
 
-def main():
-    universe = get_all_tickers()
-    indexes = [2]
-    for i in indexes:
-        print(get_data(universe[i]))
-    return
-
-
-def get_today():
-    today = datetime.datetime.now()
-    date_today = str(today.year)
-    if today.month < 10:
-        date_today += '-0' + str(today.month) + '-'
+def format_date(date):
+    formatted = str(date.year)
+    if date.month < 10:
+        formatted += '-0' + str(date.month) + '-'
     else:
-        date_today += '-' + str(today.month) + '-'
-    if today.day < 10:
-        date_today += '0' + str(today.day)
+        formatted += '-' + str(date.month) + '-'
+    if date.day < 10:
+        formatted += '0' + str(date.day)
     else:
-        date_today += str(today.day)
-    return date_today
-
-
-def get_yesterday():
-    today = datetime.datetime.now()
-    yesterday = str(today.year)
-    if today.month < 10:
-        yesterday += '-0' + str(today.month) + '-'
-    else:
-        yesterday += '-' + str(today.month) + '-'
-    if today.day < 10:
-        yesterday += '0' + str(today.day)
-    else:
-        yesterday += str(today.day - 1)
-    return yesterday
-
-
-def format_date(month, day, year):
-    date = str(year)
-    if month < 10:
-        date += '-0' + str(month) + '-'
-    else:
-        date += '-' + str(month) + '-'
-    if day < 10:
-        date += '0' + str(day)
-    else:
-        date += str(day)
-    return date
+        formatted += str(date.day)
+    return formatted
 
 
 def get_all_tickers():
@@ -73,8 +43,3 @@ def get_all_tickers():
     for i in range(0,len(data)):
         tickers.append(data[i][0])
     return tickers
-
-
-if __name__ == "__main__":
-    quandl.ApiConfig.api_key = "RxidFKB69HRV8VFHbXqM"
-    main()
--- SMA.py
+++ SMA.py
@@ -1,14 +1,15 @@
 import DataPull
-
+import datetime
 
 def sma(ticker, start, end):
     data = DataPull.get_price_data(ticker, start, end)
-    avg = sum(data['Close']) / len(data['Close'])
-    return avg
+    num_days = (end - start).days
+    one_day = datetime.timedelta(1)
+    returns = 0
+    for i in range(0, num_days):
+        returns += data.loc[DataPull.format_date(start+one_day*(i+1))]['Close'] - data.loc[DataPull.format_date(start+one_day*(i))]['Close']
+    returns = returns / num_days
+    return returns
 
+print(sma('AAPL', start=datetime.datetime(2017, 1, 4, 0, 0, 0, 0), end=datetime.datetime(2017, 1, 18, 0, 0, 0, 0))) # year, month, day
 
-def main():
-    print(sma('AAPL', start = DataPull.format_date(1,1,2008), end = DataPull.format_date(2,2,2009)))
-
-if __name__ == "__main__":
-    main()
