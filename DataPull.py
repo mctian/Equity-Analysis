5b7900b3bb4418b5b683cf4c1997f848e97b4fe2
Mason Tian <masontian@nat-tvwna-inside-visitornet101-b-3971.princeton.org>
2017-11-18 02:57:23 -0500

    ver 3
    working code, checks holidays, checks weekends

--- DataPull.py
+++ DataPull.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import quandl
 import datetime
-import csv
 from datapackage import Package
+import holidays
 
 
 quandl.ApiConfig.api_key = "RxidFKB69HRV8VFHbXqM"
@@ -19,7 +19,6 @@ def get_price_data(t, start, end):
     start_date = format_date(start)
     end_date = format_date(end)
     data = quandl.get('Wiki/'+ t, start_date=start_date, end_date=end_date)
-    print(data)
     return data
 
 
@@ -34,6 +33,27 @@ def format_date(date):
     else:
         formatted += str(date.day)
     return formatted
+
+
+def market_days(start, end):
+    all_holidays = list((holidays.US(state='NY', years = datetime.datetime.today().year)+holidays.EuropeanCentralBank()).keys())
+    indexes = [1, 4, 6, 7, 11, 12, 13, 14, 18]
+    nyse_holidays = []
+    for i in indexes:
+        nyse_holidays.append(all_holidays[i])
+    days = np.busday_count(start, end, holidays=nyse_holidays)
+    return days
+
+
+def check_open(date):
+    all_holidays = list((holidays.US(state='NY', years=datetime.datetime.today().year) + holidays.EuropeanCentralBank()).keys())
+    indexes = [1, 4, 6, 7, 11, 12, 13, 14, 18]
+    for i in indexes:
+        if date == all_holidays[i]:
+            return False
+    if date.weekday() > 4:
+        return False
+    return True
 
 
 def get_all_tickers():
--- SMA.py
+++ SMA.py
@@ -1,15 +1,38 @@
 import DataPull
 import datetime
 
+
 def sma(ticker, start, end):
     data = DataPull.get_price_data(ticker, start, end)
-    num_days = (end - start).days
+    num_days = DataPull.market_days(start, end)
     one_day = datetime.timedelta(1)
     returns = 0
-    for i in range(0, num_days):
-        returns += data.loc[DataPull.format_date(start+one_day*(i+1))]['Close'] - data.loc[DataPull.format_date(start+one_day*(i))]['Close']
+    j = 0
+    for i in range(1, num_days+1):
+        if DataPull.check_open(start+one_day*i):
+            returns += data.loc[DataPull.format_date(start+one_day*i)]['Close'] - data.loc[DataPull.format_date(start+one_day*j)]['Close']
+            j = i
     returns = returns / num_days
     return returns
 
-print(sma('AAPL', start=datetime.datetime(2017, 1, 4, 0, 0, 0, 0), end=datetime.datetime(2017, 1, 18, 0, 0, 0, 0))) # year, month, day
+
+def rate_of_return(ticker, end, n):
+    start = end - datetime.timedelta(n)
+    data = DataPull.get_price_data(ticker, start, end)
+    while not DataPull.check_open(start):
+        start += datetime.timedelta(1)
+    while not DataPull.check_open(end):
+        end -= datetime.timedelta(1)
+    returns = data.loc[DataPull.format_date(end)]['Close'] - data.loc[DataPull.format_date(start)]['Close']
+    returns = returns / data.loc[DataPull.format_date(start)]['Close']
+    print("returns by holding for " + str(n) + " days: " + str(returns))
+    return returns
+
+def main():
+    print(rate_of_return("AAPL", end=datetime.datetime.today(),n=5))
+
+if __name__ == "__main__":
+    main()
+
+
 
