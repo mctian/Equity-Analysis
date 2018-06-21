b59cbba4123f2c487b3fef6da3a4c9d13c59653c
Mason Tian <mtian@princeton.edu>
2018-06-21 23:31:19 +0800

    fixed reference to database

--- DatabaseQuery.py
+++ DatabaseQuery.py
@@ -7,14 +7,15 @@ if __name__ == '__main__':
     client = MongoClient('localhost', 27017)
     db = client.stocklists
     db2 = client.stocks
-    collection = db['stocklist']
+    collection = db['Financials']
     cursor = collection.find()
     result = pd.DataFrame(list(cursor))
     t = time.time()
     for ticker in result['Tickers']:
-        collection = db[ticker]
+        collection = db2[ticker]
         cursor = collection.find()
         stock = pd.DataFrame(list(cursor))
+        print(stock.sort_values(by=['Date']))
     print(time.time() - t)
     t = time.time()
 
